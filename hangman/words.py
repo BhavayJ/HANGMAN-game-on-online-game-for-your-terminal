11ca63ecf540e445eb13dfad0b2e16705a275f56
BhavayJ <72507871+BhavayJ@users.noreply.github.com>
2020-11-08 18:23:57 +0530

    Add files via upload

---
+++ hangman/words.py
@@ -0,0 +1,20 @@
+word_list = [
+   'PUBG',
+   'COD',
+   'Valorant',
+   'Minecraft',
+   'Among us',
+   'standoff 2',
+   'outlast',
+   'outlast 2',
+   'fortnite',
+   'Pokemon go',
+   'CS Go',
+   'Farcry'
+   'assassins creed',
+   'GTA 5',
+   'limbo'
+   'Red Dead Redemption 2',
+   'Resident Evil 4',
+   'Fall Guys',
+              ]
